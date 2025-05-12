3da12f40d1a48f21107afecf840c7a8d036d9513
khamis <mohamedkhamis20045@gmail.com>
2025-05-13 01:54:02 +0300

    Refactor GameScreen - Replaced buttons with canvas - Grid Line Drawing

--- gui/game.py
+++ gui/game.py
@@ -1,48 +1,56 @@
 import customtkinter as ctk
+import tkinter as tk
 from gui.base import BaseScreen
 
 
 class GameScreen(BaseScreen):
     def __init__(self, parent_frame, M, N, game_mode):
         super().__init__(parent_frame, fg_color="#363e47", width=800, height=600)
-        self.title_label = ctk.CTkLabel(self, text="Game Mode: " + game_mode, font=ctk.CTkFont("Arial", size=32, weight="bold"))
-        self.title_label.place(relx=0.5, rely=0.15, anchor=ctk.CENTER)
-        self.game_mode = game_mode
-        self.M = M
-        self.N = N
-        self.turn = 1
 
-        self.frame = ctk.CTkFrame(self, width=600, height=400, fg_color="#eaeaea")
-        self.frame.place(relx=0.5, rely=0.53, anchor=ctk.CENTER)
+        self.M, self.N = M, N
+        self.cell_size = 40
+        self.board = [[0] * N for _ in range(M)]
+        ctk.CTkLabel(
+            self,
+            text=f"Game Mode: {game_mode}",
+            font=ctk.CTkFont("Arial", 28, "bold"),
+            text_color="white"
+        ).pack(pady=20)
 
+        # Canvas setup
+        w = N * self.cell_size +5
+        h = M * self.cell_size +5
 
-        self.buttons = []
+        self.canvas = tk.Canvas(self, bg="#363e47", width=w, height=h, highlightthickness=0)
+        self.canvas.pack()
+        self.draw_grid()
+        #left click
+        self.canvas.bind("<Button-1>", self.on_click)
 
-        for i in range(M):
-            row = []
-            for j in range(N):
-                button = ctk.CTkButton(
-                    self.frame,
-                    text="",
-                    width=600/M,
-                    height=400/N,
-                    font=ctk.CTkFont("Arial", size=16, weight="bold"),
-                    fg_color="#363e47",
-                    text_color="#ffffff",
-                    corner_radius=2,
-                    command=lambda x=i, y=j: self.get_input(x, y)
-                )
-                button.grid(row=i, column=j, padx=2, pady=2,sticky="nsew")
-                row.append(button)
-            self.buttons.append(row)
+    def draw_grid(self):
+        for i in range(self.M):
+            y =  i * self.cell_size
+            x1 = 0
+            x2 = self.cell_size * (self.N - 1)
+            self.canvas.create_line(x1, y, x2, y, fill="#b7d2f1")
 
-    def get_input(self, x, y):
-        if self.game_mode == "AI vs AI":
-            return
-        if self.buttons[x][y].cget("text") == "":
-            if self.turn == 1:
-                self.buttons[x][y].configure(text= "W")
-                self.turn = 2
-            else:
-                self.buttons[x][y].configure(text= "B")
-                self.turn = 1
+        for j in range(self.N):
+            x = j * self.cell_size
+            y1 = 0
+            y2 = self.cell_size * (self.M - 1)
+            self.canvas.create_line(x, y1, x, y2, fill="#ECEFCA")
+
+    def on_click(self, event):
+        col = round((event.x) / self.cell_size)
+        row = round((event.y) / self.cell_size)
+
+        #validate
+        if 0 <= row < self.M and 0 <= col < self.N and self.board[row][col] == 0:
+            self.board[row][col] = 1
+            self.draw_point(row, col, "white")
+
+    def draw_point(self, row, col, color):
+        x = col * self.cell_size
+        y = row * self.cell_size
+        r = int(self.cell_size / 2) - 6
+        self.canvas.create_oval(x - r, y - r, x + r, y + r, fill = color, outline="black")
