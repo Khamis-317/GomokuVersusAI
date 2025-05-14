41a640fca04c32a7d8bb65de0b23570bff34f548
khamis <mohamedkhamis20045@gmail.com>
2025-05-14 20:38:34 +0300

    feat:display-endgame-status

--- gui/game.py
+++ gui/game.py
@@ -15,6 +15,7 @@ class GameScreen(BaseScreen):
         self.game_started = False
         self.turn = 1
         self.game_mode = game_mode
+
         for i in range(ROWS):
             row = [0] * COLS
             self.board.append(row)
@@ -26,6 +27,11 @@ class GameScreen(BaseScreen):
             font=ctk.CTkFont("Arial", 28, "bold"),
             text_color="white"
         ).pack(pady=20)
+
+
+        #winning and draw Labels
+        self.winner_label = ctk.CTkLabel(self, fg_color="transparent")
+        self.draw_label = ctk.CTkLabel(self, fg_color="transparent")
 
         #buttons
         button_frame = ctk.CTkFrame(self, fg_color="#363e47")
@@ -72,11 +78,12 @@ class GameScreen(BaseScreen):
 
         #Players
         if game_mode == "AI vs AI":
-            self.ai_player1 = "Minimax" #Going to change
-            self.ai_player2 = "Alpha Beta Pruning" #Going to change
+            self.ai_player1 = "Alpha Beta Pruning" #Going to change
+            self.ai_player2 = "Minimax" #Going to change
         else:
-            self.human_player = "" #No need for change
+            self.human_player = "Human" #No need for change
             self.ai_player = "Minimax" #Going to change
+
 
     def click(self, event):
         if self.game_started:
@@ -92,16 +99,19 @@ class GameScreen(BaseScreen):
                     if win is not None:
                         self.game_started = False
                         self.canvas.create_line(win[0][0], win[0][1], win[1][0], win[1][1], fill="yellow", width=5)
+                        self.display_winner()
                     else:
-                        self.turn = 2
-                        self.after(300, self.ai_move)
+                        if (self.is_board_full()):
+                            self.game_started = False
+                            self.display_draw_message()
+                        else:
+                            self.turn = 2
+                            self.after(300, self.ai_move)
         else:
             print("Game is finished!")
 
 
     def ai_move(self):
-        if(self.is_board_full()):
-            self.game_started = False
         if not self.game_started:
             print("Game is finished!")
             return
@@ -117,12 +127,17 @@ class GameScreen(BaseScreen):
 
         win = self.check_win(row, col)
         if win is None:
-            self.turn = 1 if self.turn == 2 else 2 #At human vs. ai mode this line behaves the same as turn = 1(human)
-            if self.game_mode == "AI vs AI":
-                self.after(300, self.ai_move) #recurse with another AI algo
+            if (self.is_board_full()):
+                game_started = False
+                self.display_draw_message()
+            else:
+                self.turn = 1 if self.turn == 2 else 2 #At human vs. ai mode this line behaves the same as turn = 1(human)
+                if self.game_mode == "AI vs AI":
+                    self.after(300, self.ai_move) #recurse with another AI algo
         else:
             self.game_started = False
             self.canvas.create_line(win[0][0], win[0][1], win[1][0], win[1][1], fill="yellow", width=5)
+            self.display_winner()
 
     def start_game(self):
         self.game_started = True
@@ -141,8 +156,11 @@ class GameScreen(BaseScreen):
         self.canvas.delete("all")
         self.draw_grid()
         self.turn = 1
+        self.winner_label.pack_forget()
+        self.draw_label.pack_forget()
         self.back_button.configure(state="normal")
         self.reset_button.configure(state="disabled")
+        self.round_count = 0
 
     def back_to_menu(self):
         from gui.start_menu import StartMenu
@@ -210,6 +228,39 @@ class GameScreen(BaseScreen):
             y2 = self.cell_size * (self.M - 1) + self.margin
             self.canvas.create_line(x, y1, x, y2, fill="#b7d2f1")
 
+
+    #display game state
+    def display_winner(self):
+        player1 = None
+        player2 = None
+        if self.game_mode == "Human vs AI":
+            player1 = self.human_player
+            player2 = self.ai_player
+        else:
+            player1 = self.ai_player1
+            player2 = self.ai_player2
+        self.winner_label.configure(
+            text=f"Winner:\n {player1 if self.turn == 1 else player2}",
+            font=ctk.CTkFont("Arial", 22, "bold"),
+            width=70,
+            height=50,
+            text_color="black",
+            fg_color="#D4C9BE",
+            corner_radius=10
+        )
+        self.winner_label.pack()
+
+    def display_draw_message(self):
+        self.draw_label.configure(
+            text="Draw!",
+            font=ctk.CTkFont("Arial", 22, "bold"),
+            width=70,
+            height=50,
+            text_color="black",
+            fg_color="#D4C9BE",
+            corner_radius=10
+        )
+        self.draw_label.pack()
 
     #Temp Function
     def dummy(self):
